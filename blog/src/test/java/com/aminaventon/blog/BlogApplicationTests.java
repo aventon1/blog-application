4113cba2e7856bc50c1ae6fb3c62cfd482d51885
aventon1 <aaventon@email.com>
2024-09-02 01:01:33 -0400

    -added abbreviated text to posts on home page - added controller test

--- blog/src/test/java/com/aminaventon/blog/BlogApplicationTests.java
+++ blog/src/test/java/com/aminaventon/blog/BlogApplicationTests.java
@@ -1,15 +1,23 @@
 package com.aminaventon.blog;
 
+import com.aminaventon.blog.controller.MainController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class BlogApplicationTests {
 
+	@Autowired
+	private MainController mainController;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(mainController).isNotNull();
 	}
 
 }
