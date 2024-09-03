251ced0776ee3c09bb07707ae7781ce1f214508e
aventon1 <aaventon@email.com>
2024-09-03 05:31:56 -0400

    -added tests for findByEmail(), findByFirstName(), findByLastName(), save()

--- blog/src/test/java/com/aminaventon/blog/BlogApplicationTests.java
+++ blog/src/test/java/com/aminaventon/blog/BlogApplicationTests.java
@@ -1,11 +1,19 @@
 package com.aminaventon.blog;
 
+import com.aminaventon.blog.model.Post;
 import com.aminaventon.blog.model.User;
+import com.aminaventon.blog.repo.PostRepository;
 import com.aminaventon.blog.repo.UserRepository;
+import com.aminaventon.blog.service.PostServiceImpl;
+import com.aminaventon.blog.service.UserServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -17,24 +25,138 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class BlogApplicationTests {
 
 	@Autowired
+	private UserServiceImpl userService;
+
+	@Autowired
 	private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private PostServiceImpl postServiceImpl;
+
+
 	/**
-	 * This method tests query findByEmail() in UserRespositoru
+	 * This method tests query findByEmail() in UserRepository
 	 * @throws NullPointerException
 	 */
-	@Test
-	void testFindByEmail() throws NullPointerException {
+	@ParameterizedTest
+	@ValueSource(strings = {"test@test.com"})
+	void testFindByEmail(String email) throws NullPointerException {
+		// create user
 		User user = new User();
 
-		user.setFirstName("TestUser");
-		user.setLastName("TestUser");
+		user.setFirstName("TestUserFirstName");
+		user.setLastName("TestUserLastName");
+		user.setEmail(email);
+		user.setPassword("password");
+
+		userRepository.save(user);
+
+
+		// Check if the repository found the user by email
+		assertThat(email).isEqualTo(userRepository.findByEmail(user.getEmail()).getEmail());
+
+		userRepository.delete(user);
+	}
+
+	/**
+	 * This method tests query findByFirstName() in UserRepository
+	 * @throws NullPointerException
+	 */
+	@ParameterizedTest
+	@ValueSource(strings = {"TestUserFirstName"})
+	void testFindByFirstName(String firstName) throws NullPointerException {
+
+		// create user
+		User user = new User();
+
+		user.setFirstName(firstName);
+		user.setLastName("TestUserLastName");
 		user.setEmail("test@test.com");
 		user.setPassword("password");
 
 		userRepository.save(user);
 
-		assertThat(user.getEmail()).isEqualTo(userRepository.findByEmail(user.getEmail()).getEmail());
+		List<User> users = userRepository.findByFirstName(firstName);
+
+		for (User u : users) {
+			// Check if the repository found the user by first name
+			assertThat(u.getFirstName()).isEqualTo(firstName);
+		}
+
+		userRepository.delete(user);
 	}
 
+	/**
+	 * This method tests query findByLastName() in UserRepository
+	 * @throws NullPointerException
+	 */
+	@ParameterizedTest
+	@ValueSource(strings = {"TestUserLastName"})
+	void testFindByLastName(String lastName) throws NullPointerException {
+
+		// create user
+		User user = new User();
+
+		user.setFirstName("TestUserFirstName");
+		user.setLastName(lastName);
+		user.setEmail("test@test.com");
+		user.setPassword("password");
+
+		userRepository.save(user);
+
+		List<User> users = userRepository.findByLastName(lastName);
+
+		for (User u : users) {
+			// Check if the repository found the user by first name
+			assertThat(u.getLastName()).isEqualTo(lastName);
+		}
+
+		userRepository.delete(user);
+	}
+
+	/**
+	 * This method tests save() in UserServiceImpl
+	 * @throws NullPointerException
+	 */
+	@Test
+	void testSaveUser() throws NullPointerException {
+		// create user
+		User user = new User();
+
+		user.setId(100L);
+		user.setFirstName("TestUserFirstName");
+		user.setLastName("TestUserLastName");
+		user.setEmail("email");
+		user.setPassword("password");
+
+		userService.save(user);
+
+		// Check if user was saved
+		assertThat(userRepository.findById(100L) != null).isTrue();
+
+		userRepository.delete(user);
+	}
+
+	/**
+	 * This method tests save() in PostServiceImpl
+	 * @throws NullPointerException
+	 */
+	@Test
+	void testSavePost() throws NullPointerException {
+		// create post
+		Post post = new Post();
+		post.setId(100L);
+		post.setTitle("TestPostTitle");
+		post.setContent("TestPostContent");
+
+		postServiceImpl.savePost(post);
+
+		// Check if post was saved
+		assertThat(postRepository.findById(100L) != null).isTrue();
+
+		postRepository.delete(post);
+	}
 }
