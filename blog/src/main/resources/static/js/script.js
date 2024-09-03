c116fb516f8093d58a7fc0b4ca6ddfa00ed321c3
aventon1 <aaventon@email.com>
2024-09-02 21:38:45 -0400

    -added javascript to home page

---
+++ blog/src/main/resources/static/js/script.js
@@ -0,0 +1,7 @@
+// alert box for home page
+alert("Welcome to My Polish Blog. Enjoy your stay :)");
+
+// changes text color
+function changeColor(element, color) {
+    element.style.color = color;
+}
