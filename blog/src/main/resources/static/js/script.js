c7bb3afcb42dc83ca79b31e754b498467e179e41
aventon1 <aaventon@email.com>
2024-09-02 21:50:47 -0400

    -added sessionStorage for alert

--- blog/src/main/resources/static/js/script.js
+++ blog/src/main/resources/static/js/script.js
@@ -1,5 +1,8 @@
-// alert box for home page
-alert("Welcome to My Polish Blog. Enjoy your stay :)");
+// alert box for home page, uses sessionStorage for each session
+if(sessionStorage.getItem('showAlert') != "false"){
+    alert("Welcome to My Polish Blog. Enjoy your stay :)");
+    sessionStorage.setItem('showAlert', "false");
+}
 
 // changes text color
 function changeColor(element, color) {
